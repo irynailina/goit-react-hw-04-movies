39d945f2383211cb290f13507e2b2147b7503d03
irynailina <irynailina@gmail.com>
2020-05-12 22:54:14 +0300

    App became functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component, lazy, Suspense } from "react";
-import * as moviesAPI from "./services/Api";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 
@@ -8,51 +7,19 @@ const AsyncMoviesPage = lazy(() => import("./pages/MoviesPage" /* movies-page */
 const AsyncMovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage" /* movie-details-page */));
 const AsyncErrorPage = lazy(() => import("./pages/ErrorPage" /* error-page */));
 
-class App extends Component {
-  state = {
-    error: null,
-    casts: [],
-    reviews: [],
-  };
-
-  getMovieCast = (id) => {
-    moviesAPI.fetchMovieCast(id).then(({ data }) =>
-      this.setState({
-        casts: [...data.cast],
-      })
-    );
-  };
-
-  getMovieReviews = (id) => {
-    moviesAPI.fetchMovieReviews(id, 1).then(({ data }) =>
-      this.setState({
-        reviews: [...data.results],
-      })
-    );
-  };
-
-  handleQueryChange = (e) => {
-    e.persist();
-    this.setState({
-      query: e.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <Navigation />
-        <Suspense fallback={<h2>Loading...</h2>}>
-          <Switch>
-            <Route exact path="/" component={AsyncHome}></Route>
-            <Route exact path="/movies" component={AsyncMoviesPage} />
-            <Route path="/movies/:id" component={AsyncMovieDetailsPage} />
-            <Route component={AsyncErrorPage} />
-          </Switch>
-        </Suspense>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+  <Navigation />
+  <Suspense fallback={<h2>Loading...</h2>}>
+    <Switch>
+      <Route exact path="/" component={AsyncHome}></Route>
+      <Route exact path="/movies" component={AsyncMoviesPage} />
+      <Route path="/movies/:id" component={AsyncMovieDetailsPage} />
+      <Route component={AsyncErrorPage} />
+    </Switch>
+  </Suspense>
+</>
+);
 
 export default App;
+
